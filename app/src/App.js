dbd138bd70e3a372b0a35218f275b576ceafd08f
Damian Jose Chocron martinez <z3n4.cs@gmail.com>
2021-09-30 15:25:19 +0200

    insert MapEventLocationFound in App

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import MapPopupWithMarket from './components/maps/MapPopupWithMarket';
+import MapEventLocationFound from './components/maps/MapEventLocationFound';
 
 const StyledApp = styled.div`
   height: 100%;
@@ -16,7 +17,6 @@ const StyledApp = styled.div`
   }
 `
 
-
 const App = () => {
 
   const height = '50vh';
@@ -29,6 +29,11 @@ const App = () => {
       <div className="mapWrapper">
         <h1>Map popup with market</h1>
         <MapPopupWithMarket height={height} position={position} messagePopup={messagePopup} zoom={zoom} />
+      </div>
+
+      <div className="mapWrapper">
+        <h1>Map with event location found on click</h1>
+        <MapEventLocationFound height={height} position={position} messagePopup={messagePopup} zoom={zoom} />
       </div>
     </StyledApp>
   );
