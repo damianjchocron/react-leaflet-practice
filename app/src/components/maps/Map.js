fae5acb9bbfac82287dff44ee81150aad6cb6a6b
Damian Jose Chocron martinez <z3n4.cs@gmail.com>
2021-09-30 15:17:25 +0200

    create Map component

---
+++ app/src/components/maps/Map.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { MapContainer, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+
+const StyledMap = styled.div`
+    .leaflet-container{
+        height: ${props => props.height ? props.height : '25vh'}; 
+    }
+`
+
+const Map = ({ height, position, zoom, CustonMarket }) => {
+    return (
+        <StyledMap height={height}>
+            <MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {CustonMarket}
+            </MapContainer>
+        </StyledMap>
+    )
+}
+
+export default Map;
+
+Map.propTypes = {
+    height: PropTypes.string,
+    position: PropTypes.array.isRequired,
+    zoom: PropTypes.number.isRequired,
+    CustonMarket: PropTypes.element
+}
