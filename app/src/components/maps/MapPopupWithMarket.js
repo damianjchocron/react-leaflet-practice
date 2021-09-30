df1180e26b684041ce430a4a8f38f9066c4b0031
Damian Jose Chocron martinez <z3n4.cs@gmail.com>
2021-09-30 15:30:29 +0200

    replace MapContainer by Map in MapPopupWithMarket.

--- app/src/components/maps/MapPopupWithMarket.js
+++ app/src/components/maps/MapPopupWithMarket.js
@@ -1,29 +1,25 @@
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css';
+import Map from './Map';
+import { Marker, Popup } from 'react-leaflet'
 
-const StyledMap = styled.div`
-    .leaflet-container{
-        height: ${props => props.height ? props.height : '25vh'}; 
-    }
-`
+const PopupWithMarket = ({ position, messagePopup }) => {
+    return (
+        <Marker position={position}>
+            <Popup>
+                {messagePopup}
+            </Popup>
+        </Marker>
+    )
+}
 
 const MapPopupWithMarket = ({ height, position, messagePopup, zoom }) => {
     return (
-        <StyledMap height={height}>
-            <MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={position}>
-                    <Popup>
-                        {messagePopup}
-                    </Popup>
-                </Marker>
-            </MapContainer>
-        </StyledMap>
+        <Map height={height}
+            position={position}
+            zoom={zoom}
+            scrollWheelZoom={false}
+            CustonMarket={<PopupWithMarket position={position} messagePopup={messagePopup} />}
+        />
     )
 }
 
