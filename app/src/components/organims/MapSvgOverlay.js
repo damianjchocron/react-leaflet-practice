f5b025e1daa0547b6eb401caff729bd3c9e12273
Damian Jose Chocron martinez <z3n4.cs@gmail.com>
2021-10-01 13:13:07 +0200

    create MapSvgOverlay component

---
+++ app/src/components/organims/MapSvgOverlay.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import Map from '../molecules/Map';
+import SvgOverlay from '../molecules/SvgOverlay';
+
+const MapSvgOverlay = ({ height, position, messagePopup, zoom }) => {
+    /* TODOD: add state for the props */
+    const bounds = [
+        [51.49, -0.08],
+        [51.5, -0.06],
+    ]
+
+    return (
+        <Map height={height}
+            position={position}
+            zoom={zoom}
+            scrollWheelZoom={false}
+            CustonMarket={<SvgOverlay messagePopup={messagePopup} bounds={bounds} />}
+        />
+    )
+}
+
+export default MapSvgOverlay;
+
+MapSvgOverlay.propTypes = {
+    height: PropTypes.string,
+    position: PropTypes.array.isRequired,
+    messagePopup: PropTypes.string.isRequired,
+    zoom: PropTypes.number.isRequired
+}
