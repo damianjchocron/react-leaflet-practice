8af0887e51d409de6cf4f2b57ae4c0b0974e61b2
Damian Jose Chocron martinez <z3n4.cs@gmail.com>
2021-10-19 14:30:32 +0200

    insert MapExternalState in App

--- app/src/App.js
+++ app/src/App.js
@@ -8,6 +8,8 @@ import MapSvgOverlay from './components/organims/MapSvgOverlay';
 import MapLayersControl from './components/organims/MapLayersControl';
 import MapBlinkingPan from './components/organims/MapBlinkingPane';
 import MapDraggableMarket from './components/organims/MapDraggableMarket';
+import MapAnimatedPanning from './components/organims/MapAnimatedPanning';
+import MapExternalState from './components/organims/MapExternalState';
 
 const StyledApp = styled.div`
   height: 100%;
@@ -59,16 +61,22 @@ const App = () => {
 
       <div className="mapWrapper">
         <h1>Map with Blinking Pane</h1>
-        <MapBlinkingPan height={height} position={position} messagePopup={messagePopup} zoom={zoom} />
+        <MapBlinkingPan height={height} position={position} zoom={zoom} />
       </div>
 
       <div className="mapWrapper">
         <h1>Map with Draggable Maket</h1>
+        <MapDraggableMarket height={height} position={position} zoom={zoom} />
+      </div>
+
       <div className="mapWrapper">
         <h1>Map Animated Panning</h1>
         <MapAnimatedPanning height={height} position={position} zoom={zoom} />
       </div>
 
+      <div className="mapWrapper">
+        <h1>Map External State</h1>
+        <MapExternalState height={height} position={position} zoom={zoom} />
       </div>
 
     </StyledApp>
