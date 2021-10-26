3c7c18bb503f7e02a9fbe73ba667c0f4234fda0b
Damian Jose Chocron martinez <z3n4.cs@gmail.com>
2021-10-26 13:43:06 +0200

    insert condition to render DisplayPosition in MapExternalState

--- app/src/components/organims/MapExternalState.js
+++ app/src/components/organims/MapExternalState.js
@@ -4,18 +4,19 @@ import Map from "../molecules/Map";
 import DisplayPosition from "../molecules/DisplayPosition";
 
 const MapExternalState = ({ position, heigth, zoom }) => {
-    const [map, setMap] = useState(null)
+    const [map, setMap] = useState();
 
     return (
-        <div>
-            <DisplayPosition center={position} map={map} zoom={zoom} />
+        <>
+            {/* This condicion is becouse map at first time is null and set after create Map component */}
+            {map ? <DisplayPosition center={position} map={map} zoom={zoom} /> : null}
             <Map
                 heigth={heigth}
                 position={position}
                 zoom={zoom}
                 whenCreated={setMap}>
             </Map>
-        </div>
+        </>
     )
 }
 
